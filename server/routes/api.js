9d63046dd6df705c1db3997f0bf162ae70d653e7
fxrhxn <farhanisalive@gmail.com>
2023-05-31 14:48:10 -0400

    testing commit

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/hello', (req, res) => {
-  res.send({ express: 'Sent from server' });
+  res.send({ express: 'Sent from server, testing the commit feature.' });
 });
 
 module.exports = router;
